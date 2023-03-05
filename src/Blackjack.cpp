6c52bfbd32e08977142fc0dea637a21cd7abc830
giorgossofronas <george@sofronas.gr>
2023-03-05 12:15:53 +0200

    initial version of multiplayer

--- src/Blackjack.cpp
+++ src/Blackjack.cpp
@@ -1,27 +1,61 @@
 #include "Blackjack.hpp"
 
+#define MAX_PLAYERS 4
+
 Blackjack::Blackjack() {
     // intro
     std::cout << "Welcome to Blackjack!" << endl << endl;
-
     //shuffle the deck
     deck.shuffle_deck();
 }
 
+Blackjack::~Blackjack() {
+    // free players at table
+    for (auto p : players)
+        delete p;
+}
+
 void Blackjack::start() {
-    // get the player's name
-    std::cout << "Enter your name: ";
-    string name;
-    getline(std::cin, name);
-    player1.set_name(name);
-    std::cout << player1 << " has just sat down. Starting money: " << player1.get_money() << endl << endl;
+    // starting num of players sitting
+    int n_players;
+    do {
+        std::cout << "How many players want a sit at the table ? (1-4)" << endl;
+        std::cin >> n_players;
+    } while (n_players < 1 || n_players > MAX_PLAYERS);
+    std::cout << endl;
+
+    // info for players
+    for (int i = 0; i < n_players; i++) {
+
+        Player* new_player = new Player;
+
+        // get the player's name
+        std::cout << "Player " << i+1 << " - Enter your name: ";
+        string name;
+
+        ////////////////////////////////////////////////////////////////////////////
+        // UNKNOWN ERROR; If we don't put this
+        // then we don't input the name of Player 1
+        if (i == 0)
+            getline(std::cin, name);
+        ////////////////////////////////////////////////////////////////////////////
+
+        getline(std::cin, name);
+        new_player->set_name(name);
+        std::cout << *new_player << " has just sat down. Starting money: " << new_player->get_money() << endl << endl;
+    
+        players.push_back(new_player);
+    }
 }
 
 void Blackjack::round_start() {
-    // player inputs bet at the start of the round
-    std::cout << player1 << "'s money: " << player1.get_money() << endl;
-    player1.input_bet();
-    std::cout << endl;
+    // all players draw/play before Dealer
+    for (auto player : players) {
+        // player inputs bet at the start of the round
+        std::cout << *player << "'s money: " << player->get_money() << endl;
+        player->input_bet();
+        std::cout << endl;
+    }
 
     // dealer always draws the first card
     dealer.draw(deck);
@@ -30,80 +64,82 @@ void Blackjack::round_start() {
 }
 
 void Blackjack::player_turn() {
-    std::cout << "-----" << player1 <<"'s Turn-----" << endl;
+    for (auto player : players) {
+        std::cout << "-----" << *player <<"'s Turn-----" << endl;
 
-    bool stand = false;
+        bool stand = false;
 
-    // player automatically draws his first 2 cards
-    std::cout << player1 << " draws!" << endl;
-    player1.draw(deck);
-    player1.draw(deck);
-    player1.print_hand();
+        // player automatically draws his first 2 cards
+        std::cout << *player << " draws!" << endl;
+        player->draw(deck);
+        player->draw(deck);
+        player->print_hand();
 
-    // check for blackjack
-    if (player1.get_hand_value() == 21) {
-        std::cout << "Blackjack! " << player1 << " stands at 21!" << endl << endl;
-        stand = true;
-    }
+        // check for blackjack
+        if (player->get_hand_value() == 21) {
+            std::cout << "Blackjack! " << *player << " stands at 21!" << endl << endl;
+            stand = true;
+        }
 
-    // get player's choice
-    while (!stand) {
-        // ask if player wants to hit or stand
-        string choice;
-        if (player1.get_cards_in_hand() == 2)   // player can only double after being dealt the first 2 cards
-            std::cout << "What will you do?" << endl << "1. Hit" << endl << "2. Stand" << endl << "3. Double" << endl;
-        else
-            std::cout << "What will you do?" << endl << "1. Hit" << endl << "2. Stand" << endl;
-        std::cin >> choice;
+        // get player's choice
+        while (!stand) {
+            // ask if player wants to hit or stand
+            string choice;
+            if (player->get_cards_in_hand() == 2)   // player can only double after being dealt the first 2 cards
+                std::cout << "What will you do?" << endl << "1. Hit" << endl << "2. Stand" << endl << "3. Double" << endl;
+            else
+                std::cout << "What will you do?" << endl << "1. Hit" << endl << "2. Stand" << endl;
+            std::cin >> choice;
 
 
-        // if hit was chosen
-        if (choice == "1") {
-            std::cout << player1 << " hits!" << endl;
-            player1.draw(deck);
-            player1.print_hand();
+            // if hit was chosen
+            if (choice == "1") {
+                std::cout << *player << " hits!" << endl;
+                player->draw(deck);
+                player->print_hand();
 
-            // check for blackjack
-            if (player1.get_hand_value() == 21) {
-                std::cout << "Blackjack! ";
+                // check for blackjack
+                if (player->get_hand_value() == 21) {
+                    std::cout << "Blackjack! ";
+                    stand = true;
+                }
+            }
+            // if stand was chosen
+            else if (choice == "2")
                 stand = true;
+            // if double was chosen
+            else if (choice == "3" && player->get_cards_in_hand() == 2) {
+                // check if bet is legal
+                if (player->get_bet() > player->get_money())
+                    std::cout << "Not enough money to double bet." << endl << endl; 
+                else {
+                    std::cout << *player << " doubles!" << endl;
+                    player->double_bet();
+                    player->draw(deck);
+                    player->print_hand();
+
+                    stand = true;   // player automatically stands after doubling
+                }
             }
-        }
-        // if stand was chosen
-        else if (choice == "2")
-            stand = true;
-        // if double was chosen
-        else if (choice == "3" && player1.get_cards_in_hand() == 2) {
-            // check if bet is legal
-            if (player1.get_bet() > player1.get_money())
-                std::cout << "Not enough money to double bet." << endl << endl; 
+            // if invalid choice
             else {
-                std::cout << player1 << " doubles!" << endl;
-                player1.double_bet();
-                player1.draw(deck);
-                player1.print_hand();
+                std::cout << "Invalid choice." << endl;
+                continue;
+            }
 
-                stand = true;   // player automatically stands after doubling
+            // check for busted
+            if (player->get_hand_value() > 21) {
+                std::cout << "Busted!" << endl;
+                stand = true;
+                player->set_busted(true);
             }
         }
-        // if invalid choice
-        else {
-            std::cout << "Invalid choice." << endl;
-            continue;
-        }
 
-        // check for busted
-        if (player1.get_hand_value() > 21) {
-            std::cout << "Busted!" << endl;
-            stand = true;
-            player1.set_busted(true);
-        }
+        if (!player->is_busted())
+            std::cout << *player << " stands at " << player->get_hand_value() << endl;
+        
+        std::cout << endl;
     }
-
-    if (!player1.is_busted())
-        std::cout << player1 << " stands at " << player1.get_hand_value() << endl;
-    
-    std::cout << endl;
 }
 
 void Blackjack::dealer_turn() {
@@ -129,23 +165,55 @@ void Blackjack::dealer_turn() {
 }
 
 void Blackjack::round_end() {
-    // print the winner
-    if (dealer.is_busted() && player1.is_busted())
-        std::cout << "No one wins..." << endl;
-    else if (player1.is_busted() || (!dealer.is_busted() && dealer.get_hand_value() >= player1.get_hand_value()))
-        std::cout << "Dealer wins..." << endl;
-    else {
-        player1.add_money(2 * player1.get_bet());
-        std::cout << player1 <<  " wins!" << endl;
+    // if dealer lost then all players (non-busted) win
+    if (dealer.is_busted()) {
+        for (auto player : players) {
+            if (!player->is_busted()) {
+                player->add_money(2 * player->get_bet());
+                std::cout << *player <<  " wins!" << endl;
+            }
+        }
     }
+    else {
+        for (auto player : players) {
+            if (!player->is_busted()) {
+                if (dealer.get_hand_value() >= player->get_hand_value())
+                    std::cout << "Dealer wins..." << endl;
+                else {
+                    std::cout << *player <<  " wins!" << endl;
+                    player->add_money(2 * player->get_bet());
+                }
+            }
+            else
+                std::cout << "Dealer wins..." << endl;
+        }
+    }
+
+    // // print the winner
+    // if (dealer.is_busted() && player->is_busted())
+    //     std::cout << "No one wins..." << endl;
+    // else if (player->is_busted() || (!dealer.is_busted() && dealer.get_hand_value() >= player->get_hand_value()))
+    //     std::cout << "Dealer wins..." << endl;
+    // else {
+    //     player->add_money(2 * player->get_bet());
+    //     std::cout << *player <<  " wins!" << endl;
+    // }
 }
 
 bool Blackjack::end() {
-    // check if out of money
-    if (player1.get_money() <= 0) {
-        std::cout << player1 << " is out of money. Game ends." << endl;
-        return true;
+    // check if some players are out of money => delete them
+    for (int i = 0; i < (int)players.size(); i++) {
+        if (players[i]->get_money() <= 0) {
+            std::cout << *players[i] << " is out of money. " << *players[i] << " leaves the table." << endl;
+            // remove player
+            delete players[i];
+            players.erase(players.begin() + i);
+        }
     }
+
+    // if all players are eliminated then the game ends
+    if (players.size() <= 0)
+        return true;
 
     // check if player wants to play again
     string play;
@@ -155,14 +223,14 @@ bool Blackjack::end() {
         std::cout << "Enter y or n: ";
         getline(std::cin, play);
     }
-    
+    std::cout << endl;
     // if player quits, return true
     if (play == "n" || play == "N")
         return true;
+    
     // else, reset all hands and return false 
-    else {   
-        player1.reset();
-        dealer.reset();
-        return false;
-    }
+    for (auto player : players)
+        player->reset();
+    dealer.reset();
+    return false;
 }
